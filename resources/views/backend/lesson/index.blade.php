cb376aa7d79830eb6b8861923782b2856c8de26e
MD. Shamim Ahmed <shamimahmedridoy2016@gmail.com>
2022-09-27 02:02:32 +0600

    user index $ lesson module done

---
+++ resources/views/backend/lesson/index.blade.php
@@ -0,0 +1,85 @@
+@extends('backend.layouts.master')
+
+@section('title', 'Dashboard')
+
+@section('content')
+    <div class="page-content">
+        <div class="container-fluid">
+
+            <!-- start page title -->
+            <div class="row">
+                <div class="col-12">
+                    <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                        <h4 class="mb-sm-0">Lessons</h4>
+
+                        <div class="page-title-right">
+                            <ol class="breadcrumb m-0">
+                                <li class="breadcrumb-item"><a href="javascript: void(0);">Pages</a></li>
+                                <li class="breadcrumb-item active">lesson</li>
+                            </ol>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <!-- end page title -->
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="my-3 text-end">
+                        <a href="{{ route('lesson.create') }}" class="btn btn-primary waves-effect waves-light">Add New</a>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <!-- Tables Without Borders -->
+                    <table class="table table-bordered ">
+                        <thead>
+                            <tr>
+                                <th scope="col" class="text-center">#</th>
+                                <th scope="col" class="text-center">Name</th>
+                                <th scope="col" class="text-center">Content</th>
+                                <th scope="col" class="text-center">lesson_id</th>
+                                <th scope="col" class="text-center">Visibility</th>
+                                <th scope="col" class="text-center">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse ($lessons as $key=>$lesson)
+                                <tr>
+                                    <td >{{ $key+1 }}</td>
+                                    <td><a href="{{ route('lesson.show', $lesson) }}">{{ $lesson->name }}</a></td>
+                                    <td style="width: 25%">{{ $lesson->content }}</td>
+                                    <td>{{ $lesson->course_id }}</td>
+                                    <td>{{ $lesson->visibility }}</td>
+                                    <td>
+                                        <div class="hstack gap-3 fs-15 justify-content-center">
+                                            <a href="{{ route('lesson.edit', $lesson) }}" class="link-primary fs-18"><i
+                                                    class="ri-edit-fill "></i></a>
+                                            <a href="javascript:void(0);" class="link-success fs-18"><i
+                                                    class="ri-eye-line "></i></a>
+                                            <form action="{{ route('lesson.destroy', $lesson) }}" method="post"
+                                                onsubmit="return confirm('Do you really want to delete?');">
+                                                @csrf
+                                                @method('DELETE')
+
+                                                <button type="submit" class="link-danger fs-18"><i
+                                                        class="ri-delete-bin-5-line"></i></button>
+                                            </form>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @empty
+                            @endforelse
+                        </tbody>
+                    </table>
+                    <div class="mt-4">
+                        {{ $lessons->links() }}
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <!-- container-fluid -->
+
+    </div>
+@endsection
